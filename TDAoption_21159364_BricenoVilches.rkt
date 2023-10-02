76ba7bfe72b47fb680a42211af64036dcee0d4e5
LemonPatt <patricio.briceno@usach.cl>
2023-10-02 18:55:01 -0300

    Script de pruebas N°1 agregado y TDA option desarrollado

--- TDAoption_21159364_BricenoVilches.rkt
+++ TDAoption_21159364_BricenoVilches.rkt
@@ -9,5 +9,27 @@
 (define (option code message ChatbotCodeLink InitialFlowCodeLink . Keyword)
   (list code message ChatbotCodeLink InitialFlowCodeLink Keyword))
 
+(define (get-option-code option)
+  (car option))
 
-(provide option)
+(define (get-option-cbcodelink option)
+  (caddr option))
+
+(define (get-eleccion-op list-options eleccion)
+  (if (null? list-options)
+      list-options
+      (if
+       (or
+        (string=? eleccion (number->string (get-option-code (car list-options))))
+        (find-keyword-ci (list-ref (car list-options) 4) eleccion))
+       (car list-options)
+       (get-eleccion-op (cdr list-options) eleccion))))
+
+(define (find-keyword-ci list-keywords eleccion)
+  (cond
+   ((null? list-keywords) #f)
+    ((string-ci=? (string-downcase eleccion) (string-downcase (car list-keywords))) #t)
+    (else (find-keyword-ci (cdr list-keywords) eleccion))))
+
+        
+(provide option get-eleccion-op)
