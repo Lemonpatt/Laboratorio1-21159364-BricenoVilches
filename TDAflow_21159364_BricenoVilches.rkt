dd288ea120f837d4b29b383b7f57fb7d40eacdca
LemonPatt <patricio.briceno@usach.cl>
2023-10-05 22:49:03 -0300

    Añadida documentación para los TDA de flow y option

--- TDAflow_21159364_BricenoVilches.rkt
+++ TDAflow_21159364_BricenoVilches.rkt
@@ -2,41 +2,119 @@
 
 (require "TDAoption_21159364_BricenoVilches.rkt")
 
-;RF 3 Función Flow - constructor
-;Función crea una lista con los datos de un flow con ninguna o varias opciones sin repetir basandonos en su id usando una llave
-;Dominio: id (int) X name-msg (String) X Option* (0 o más opciones)
-;Recorrido: flow (list)
-
-(define (flow id name-msg . Option)
-  (list id name-msg (remove-duplicates Option #:key getflowoption-code)))
 
 
-;Función Flow - selector option code
-;Función que toma el código de una opción para despues ser usada como llave en la revisión de duplicados
+;Capa selector
+
+
+;Retorna la lista de opciones dentro de un flow
+;Dominio: flow (list)
+;Recorrido: options (list)
+
+(define (getflow-options flow)
+   (caddr flow))
+
+
+;Retorna la id de un flow
+;Dominio: flow (list)
+;Recorrido: id (int)
+
+(define (getflow-id flow)
+  (car flow))
+
+
+;Retorna la lista de opciones dentro de un flow
+;Dominio: flow (list)
+;Recorrido: options (list)
+
+(define (getflow-namemsg flow)
+   (cadr flow))
+
+
+
+;Capa Modificador
+
+
+;Toma un flow y una opción y crea un nuevo flow con la opcion añadida a la lista de opciones
 ;Dominio: option (list)
 ;Recorrido: code (int)
-(define (getflowoption-code Option)
-  (car Option))
-
-
-(define (juntar-flow-op flow option)
-  (reverse(cons option (reverse (getflow-options flow)))))
 
 (define (añadir-opcion-flow flow option)
   (cons (getflow-id flow)
         (cons (getflow-namemsg flow)
               (cons (juntar-flow-op flow option) null))))
 
+
+;Capa pertenencia
+
+;Revisa si el primer elemento de una lista se encuentra dentro de una lista de listas en la misma posición
+;Dominio: elem (list) X list-elem (list)
+;Recorrido: Boolean
+
 (define (no-duplicado? elem list-elem)
    (not (member (car elem) (map car list-elem))))
 
-(define (getflow-options flow)
-   (caddr flow))
 
-(define (getflow-id flow)
-  (car flow))
 
-(define (getflow-namemsg flow)
-   (cadr flow))
+;Otros
 
-(provide getflowoption-code añadir-opcion-flow getflow-options getflow-id no-duplicado?)
+;Función auxiliar para añadir la opcion a la lista de opciones de un flow
+;Dominio: flow (list) X option (list)
+;Recorrido: option (list)
+
+(define (juntar-flow-op flow option)
+  (sort (reverse(cons option (reverse (getflow-options flow)))) < #:key get-option-code))
+
+
+
+;Muestra las opciones de un flow elegido por el chatbot al conversar con este
+;Dominio: lista de opciones
+;Recorrido: NA
+
+(define (show-options list-options)
+  (if (null? (cdr list-options))
+      (begin
+        (display (string-append (number->string( car (car list-options))) ")" (cadr(car list-options)) "\n\n"))
+        )
+      (begin
+        (display (string-append (number->string (car (car list-options))) ")" (cadr(car list-options)) "\n\n"))
+        (show-options (cdr list-options)))))
+
+
+;Lo mismo que show-options pero de manera no recursiva para el requisito funcional 13
+;Dominio: lista de opciones
+;Recorrido: NA
+(define (show-options-norec list-options)
+  (map
+   (lambda (option)
+     (display (string-append (number->string (car option)) ")" (cadr option) "\n\n")))
+   list-options))
+
+
+;Toma todo lo importante dentro de cada opción dentro del flow y lo guarda como un string para posteriormente ser guardado en el historial del chat
+;Dominio: lista de opciones
+;Recorrido: string
+
+(define (save-options list-options)
+  (if (null? (cdr list-options))
+        (string-append (number->string( car (car list-options))) ")" (cadr(car list-options)) "\n\n")
+        (string-append (number->string( car (car list-options))) ")" (cadr(car list-options)) "\n\n"
+                       (save-options (cdr list-options)))))
+
+
+;Lo mismo que save-options pero de manera no recursiva para el requisito funcional 13
+;Dominio: lista de opciones
+;Recorrido: string
+
+(define (save-options-norec list-options)
+  (apply string-append
+   (map
+    (lambda (option)
+      (string-append
+       (number->string (car option))
+       ")" (cadr option) "\n\n"))
+    list-options)))
+
+
+
+(provide añadir-opcion-flow getflow-options getflow-id no-duplicado? show-options save-options show-options-norec save-options-norec)
--- TDAoption_21159364_BricenoVilches.rkt
+++ TDAoption_21159364_BricenoVilches.rkt
@@ -1,19 +1,27 @@
 #lang racket
 
 
-;RF 2 Función Option - constructor
-;Función crea una opción con un código único para un flow de un chatbot
-;Dominio: code (Int) X message (String) X ChatbotCodeLink (Int) X InitialFlowCodeLink (Int) X Keyword* (0 o más strings)
-;Recorrido: option (list)
+;Capa selector
 
-(define (option code message ChatbotCodeLink InitialFlowCodeLink . Keyword)
-  (list code message ChatbotCodeLink InitialFlowCodeLink Keyword))
+
+;Retorna el codigo de una opción /Usado para revisar duplicados
+;Dominio: option (list)
+;Recorrido: code (int)
 
 (define (get-option-code option)
   (car option))
 
+;Retorna el id del chatbot inicial al que dirige una opción
+;Dominio: option (list)
+;Recorrido: ChatbotInitialCodeLink (int)
+
 (define (get-option-cbcodelink option)
   (caddr option))
+
+
+;Retorna una opcion dentro de una lista en donde un string dado contenga su code o keyword
+;Dominio: lista de opciones X elección (string)
+;Recorrido: Opción elegida (list)
 
 (define (get-eleccion-op list-options eleccion)
   (if (null? list-options)
@@ -25,11 +33,43 @@
        (car list-options)
        (get-eleccion-op (cdr list-options) eleccion))))
 
+
+;Función interna de get-eleccion-op que revisa si la elección se encuentra dentro de la lista de keywords
+;Dominio: lista de keywords X elección (string)
+;Recorrido: Boolean
+
 (define (find-keyword-ci list-keywords eleccion)
   (cond
    ((null? list-keywords) #f)
     ((string-ci=? (string-downcase eleccion) (string-downcase (car list-keywords))) #t)
     (else (find-keyword-ci (cdr list-keywords) eleccion))))
 
-        
-(provide option get-eleccion-op)
+
+;Versión sin recursión de get-eleccion-op para el requisito funcional 13
+;Dominio: lista de opciones X elección (string)
+;Recorrido: Opción elegida (list)
+
+(define (get-eleccion-op-norec list-options eleccion)
+  (let ((match-eleccion-op?
+         (lambda (op)
+           (or (string=? eleccion (number->string (get-option-code op)))
+            (find-keyword-ci-norec (list-ref op 4) eleccion)))))
+  (if (null? (filter 
+           match-eleccion-op? list-options))
+      '()
+      (car(filter 
+           match-eleccion-op? list-options)))))
+
+;Versión sin recursión de la función interna de get-eleccion-op para el requisito funcional 13
+;Dominio: lista de keywords X elección (string)
+;Recorrido: Boolean
+
+(define (find-keyword-ci-norec list-keywords eleccion)
+  (let ((match-eleccion-keyword?
+         (lambda (keyword)
+           (string-ci=? (string-downcase eleccion) (string-downcase keyword)))))
+  (cond
+   ((null? (filter match-eleccion-keyword? list-keywords)) #f)
+   ((not(null?(filter match-eleccion-keyword? list-keywords))) #t))))
+
+(provide get-eleccion-op get-eleccion-op-norec get-option-code)
